c776a847749eea2072ef5e745c320fe703494889
NakkaGS <gtnakata@gmail.com>
2022-12-27 16:20:21 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+    $('#navbar-toggler').click(function(){
+        $('.navbar-collapse').slideToggle(400)
+    })
+
+    $(window).scroll(function(){
+        let pos = $(window).scrollTop();
+        if(pos >= 100){
+            $('.navbar').addClass('cng-navbar');
+        } else {
+            $('.navbar').removeClass('cng-navbar')
+        }
+    })
+
+    $(document).ready(function() {
+        $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+            disableOn: 700,
+            type: 'iframe',
+            mainClass: 'mfp-fade',
+            removalDelay: 160,
+            preloader: false,
+    
+            fixedContentPos: false
+        });
+    });
+
+    $('.team .owl-carousel').owlCarousel ({
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        dots: true,
+        nav: false, 
+        responsiveClass: true,
+        responsive: {
+            0:{
+                items: 1
+            },
+            600:{
+                items: 2
+            },
+            1000:{
+                items: 3
+            }
+        }
+    })
+
+    // faq accordion
+    $('.faq-head').each(function(){
+        $(this).click(function(){
+            $(this).next().toggleClass('show-faq-content');
+            let icon = $(this).children('span').children("i").attr('class');
+
+            if(icon == "fas fa-plus"){
+                $(this).children('span').html('<i class = "fas fa-minus"></i>');
+            } else {
+                $(this).children('span').html('<i class = "fas fa-plus"></i>');
+            }
+        });
+    });
+})
